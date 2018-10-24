4f2f00fcc4df4afc4eb558d01ebc52b9b95eff10
Bryan Venable <github@spif.com>
2018-10-24 23:25:41 +0000

    use __dirname

--- mhrlocal.js
+++ mhrlocal.js
@@ -3,7 +3,7 @@ var path = require("path");
 var util = require("util");
 var Browser = require("zombie");
 var assert = require("assert");
-var indexfile = util.format('file://%s/index.html', path.resolve(path.dirname('.')));
+var indexfile = util.format('file://%s/index.html', __dirname);
 Browser.visit(indexfile, function(error,browser) {
 	console.log(browser.html());
 });
